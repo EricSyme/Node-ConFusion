451f1c7074c3160179b97e62733cc7e02f71838d
Eric Syme <spotlightphotoseattle@gmail.com>
2019-08-24 10:08:14 -0700

    Week 2 Assignment

---
+++ models/leaders.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	image: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	designation: {
+		type: String,
+		required: true,
+	},
+	abbr: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	featured: {
+		type: Boolean,
+		default: false
+	}
+}, {
+	timestamps: true
+});
+
+//Create constant Promotions for the model
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
