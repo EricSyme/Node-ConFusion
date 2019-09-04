ca629570ae67ba3082f963158470a217aeb0d6e9
Eric Syme <spotlightphotoseattle@gmail.com>
2019-09-03 23:09:28 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '503234517159766',
+        clientSecret: '09f6ed5709266c3cdda62ac8d621e7a0'
+    }
 }
